f0927a76c8fd1ff03df289b4d4194c7148a7f37f
DmitryLasuta <lasutadima01@gmail.com>
2024-03-05 04:08:13 +0300

    feat: adds the class to interact with Valantis API

---
+++ src/api/store.ts
@@ -0,0 +1,125 @@
+import type { Product } from '@/types';
+import md5 from 'md5';
+
+type ProductExcludingId = Omit<Product, 'id'>;
+type ProductFieldNames = keyof ProductExcludingId;
+
+class SoreAPI {
+  public baseURL = 'https://api.valantis.store:41000';
+  private password = import.meta.env.VITE_STORE_API_PASSWORD;
+
+  private xAuth: string;
+
+  constructor() {
+    const timestamp = new Date().toISOString().split('T')[0].replace(/-/g, '');
+    this.xAuth = md5(`${this.password}_${timestamp}`);
+  }
+
+  /**
+   * Fetcher function to make a POST request to a specified URL.
+   *
+   * @template T The expected return type of the fetch request.
+   * @param {Object} requestBody The body of the request.
+   * @param {('get_ids' | 'get_items' | 'filter' | 'get_fields')} requestBody.action The action to be performed.
+   * @param {Object} [requestBody.params] Additional parameters for the request.
+   * @returns {Promise<T>} Returns a Promise that resolves with the result of the fetch request.
+   **/
+  private async fetcher<T>(requestBody: {
+    action: 'get_ids' | 'get_items' | 'filter' | 'get_fields';
+    params?: object;
+  }): Promise<T> {
+    const response = fetch(this.baseURL, {
+      method: 'POST',
+      headers: {
+        'X-Auth': this.xAuth,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    const data = response.then(res => res.json()) as Promise<{ result: T }>;
+    return (await data).result;
+  }
+
+  /**
+   * The method returns an ordered list of product identifiers.
+   * Detailed information about the product can be requested using the selected identifiers.
+   * By default, it returns identifiers of all available products.
+   *
+   * Parameters:
+   * @param {number} limit The number of identifiers.
+   * @param {number} offset  Offset relative to the beginning of the list.
+   * @returns {Promise<string[]>} An array of identifiers
+   */
+  public async getIdentifiers(limit: number, offset: number): Promise<string[]> {
+    const identifiers = await this.fetcher<string[]>({
+      action: 'get_ids',
+      params: { limit, offset },
+    });
+
+    const uniqueIdentifiers = identifiers.filter((id, index, self) => {
+      return self.indexOf(id) === index;
+    });
+
+    return uniqueIdentifiers;
+  }
+
+  /**
+   * The method returns an ordered list of products with all characteristics by their identifiers.
+   * Maximum: `100` records. If you need to get more than 100, you need to make a request multiple times.
+   * Without identifiers returns null
+   *
+   * Parameters:
+   * @param {string[]} identifiers - An array of identifiers
+   * @returns {Promise<Product[] | null>} If an array of identifiers is passed, returns an array of products with all characteristics
+   */
+  public async getProducts(identifiers: string[]): Promise<Product[] | null> {
+    return this.fetcher({
+      action: 'get_items',
+      params: {
+        ids: identifiers,
+      },
+    });
+  }
+
+  /**
+   * The method without parameters returns an ordered list of existing product fields.
+   * When given the field parameter, it returns an ordered list of the values of the given product field.
+   *
+   * Parameters:
+   * @param {string} params.field - The specified field
+   * @param {number} params.offset -  Offset relative to the beginning of the list
+   * @param {string} params.limit - The number of returned records
+   * @returns {Promise<Array<number | string | null>>}
+   */
+  public getFields(params?: {
+    field: ProductFieldNames;
+    offset?: number;
+    limit?: number;
+  }): Promise<Array<number | string | null>> {
+    return this.fetcher({
+      action: 'get_fields',
+      params,
+    });
+  }
+
+  /**
+   * Used to filter.
+   * Returns an ordered list of product IDs matching the specified value.
+   *
+   * Any field returned by the `getFields` method without parameters can be used as a parameter.
+   * The data type corresponding to the field must be used as the value.
+   * For the product field it will be checked if the parameter is a string.
+   * For other fields, strict matching is checked.
+   * @param {Partial<ProductExcludingId>} params Parameters for filtering.
+   * @returns {Promise<string[]>} List of product identifiers that satisfy the filtering conditions.
+   */
+  public async getFilteredProductIdentifiers(params: Partial<ProductExcludingId>): Promise<string[]> {
+    return this.fetcher({
+      action: 'filter',
+      params,
+    });
+  }
+}
+
+export const storeAPI = new SoreAPI();
